fb22b751d8db8f325d40fe212a2636cfaefed536
lbarjak <barjak.laszlo@gmail.com>
2023-03-03 22:35:57 +0100

    graph is displayed

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -15,24 +15,16 @@ class MyClass {
 
 new MyClass();
 
-		new Chart(document.getElementById("line-chart"), {
-			type : 'line',
-			data : {
-				labels : [0, "szombat", "vasárnap", "hétfő", "kedd", "szerda", 1850, 1900, 1950,
-						1999],
-				datasets : [
-						{
-							data : proba,
-							label : "Mért adatokból",
-							borderColor : "#3e95cd",
-							fill : false
-						},
-						{
-							data : [ 1, 2, 3, 4, 5, 6, 7, 8,
-									9, 10 ],
-							label : "Előrejelzésből",
-							borderColor : "#8e5ea2",
-							fill : false
-						} ]
-			}
-		});
+new Chart(document.getElementById("line-chart"), {
+	type: 'line',
+	data: {
+		labels: roomTemp1,
+		datasets: [
+			{
+				data: roomTemp1,
+				label: "Mért adatokból",
+				borderColor: "#3e95cd",
+				fill: false
+			}]
+	}
+});
