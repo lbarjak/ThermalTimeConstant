61d4cc526b3846fea1fbc40b719c2c4adc883603
lbarjak <barjak.laszlo@gmail.com>
2023-03-05 19:58:04 +0100

    tomcat 10, java 17

--- src/main/java/eu/barjak/thermaltimeconstant/ThermalTimeConstantApplication.java
+++ src/main/java/eu/barjak/thermaltimeconstant/ThermalTimeConstantApplication.java
@@ -2,12 +2,19 @@ package eu.barjak.thermaltimeconstant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThermalTimeConstantApplication {
+public class ThermalTimeConstantApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThermalTimeConstantApplication.class, args);
 	}
+	
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ThermalTimeConstantApplication.class);
+//    }
 
 }
